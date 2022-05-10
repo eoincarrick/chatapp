756487eba4c539cf23a8161fb4dc6b5fe7016d74
Eoin Carrick <22carri99@gmail.com>
2022-05-10 01:57:13 +0000

    SAEV

--- server/config/db.js
+++ server/config/db.js
@@ -2,11 +2,14 @@ const mongoose = require('mongoose');
 
 const connectDatabase = async () => {
   try {
-    const connect = mongoose.connect(`${process.env.MONGODB_URI}`);
+    const connect = await mongoose.connect(`${process.env.MONGODB_URI}`);
 
-    console.log();
+    console.log(`MongoDB Connected: ${connect.connection.host}`);
   } catch (error) {
     console.error(error);
     console.log(error.message);
+    process.exit(1);
   }
 };
+
+module.exports = { connectDatabase };
--- server/controllers/users.js
+++ server/controllers/users.js
@@ -1,11 +1,99 @@
-const getUsers = (request, response) => {};
-const postUsers = (request, response) => {};
-const putUsers = (request, response) => {};
-const deleteUsers = (request, response) => {};
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
+const User = require('../models/users');
+
+//////////////////////////////////////////////
+///////////////USER///////////////////////////
+//////////////////////////////////////////////
+
+// @desc Post user data
+// @route Method: POST ('/users')
+// @access Private && User
+const registerUser = (request, response) => {
+  try {
+  } catch (error) {
+    console.error(error);
+    console.error(error.message);
+  }
+};
+
+// @desc Post user data
+// @route Method: POST ('/users/login')
+// @access Private && User
+const loginUser = (request, response) => {
+  try {
+  } catch (error) {
+    console.error(error);
+    console.error(error.message);
+  }
+};
+
+// @desc Get user data
+// @route Method: GET ('/users/profile')
+// @access Private && User
+const userProfile = (request, response) => {
+  try {
+  } catch (error) {
+    console.error(error);
+    console.error(error.message);
+  }
+};
+
+//////////////////////////////////////////////
+///////////////ADMINISTRATOR//////////////////
+//////////////////////////////////////////////
+
+// @desc Get user data
+// @route Method: GET ('/users')
+// @access Private && Admin
+
+const getUsers = (request, response) => {
+  try {
+  } catch (error) {
+    console.error(error);
+    console.error(error.message);
+  }
+};
+
+// @desc Post user data
+// @route Method: POST ('/users')
+// @access Private && Admin
+const postUsers = (request, response) => {
+  try {
+  } catch (error) {
+    console.error(error);
+    console.error(error.message);
+  }
+};
+
+// @desc Put user data
+// @route Method: PUT ('/users/:ID')
+// @access Private && Admin
+const putUsers = (request, response) => {
+  try {
+  } catch (error) {
+    console.error(error);
+    console.error(error.message);
+  }
+};
+
+// @desc Delete user data
+// @route Method: DELETE ('/users/:ID')
+// @access Private && Admin
+const deleteUsers = (request, response) => {
+  try {
+  } catch (error) {
+    console.error(error);
+    console.error(error.message);
+  }
+};
 
 module.exports = {
   getUsers,
   postUsers,
   putUsers,
   deleteUsers,
+  registerUser,
+  loginUser,
+  userProfile,
 };
