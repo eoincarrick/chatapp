77c0705a68c1dae78498a789360693f91de26b9e
Eoin Carrick <22carri99@gmail.com>
2022-05-12 00:04:02 +0000

    card

--- client/src/components/CardFeed.jsx
+++ client/src/components/CardFeed.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from 'react';
 
 const CardFeed = () => {
-  return (
-    <div>CardFeed</div>
-  )
-}
+  const data = {
+    name: 'Christable Doe',
+    age: 21,
+    country: 'South Korea',
+    photo:
+      'https://images.unsplash.com/photo-1534528741775-53994a69daeb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjB8fHByb2ZpbGV8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60',
+  };
 
-export default CardFeed
+  const { photo } = data;
+  const css = {
+    backgroundImage: `url(${photo})`,
+    width: '100%',
+    height: '50em',
+    backgroundRepeat: 'no-repeat',
+    objectFit: 'contain',
+  };
+  return (
+    <div style={css}>
+      <h1>Hello</h1>
+    </div>
+  );
+};
+
+export default CardFeed;
